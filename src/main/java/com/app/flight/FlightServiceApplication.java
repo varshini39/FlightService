8bb5993a273b9ea98b875911a06d6dab1165ba9d
varshini39 <ragavs95@gmail.com>
2023-10-26 23:04:50 +0530

    Flight service deployed in docker container

--- src/main/java/com/app/flight/FlightServiceApplication.java
+++ src/main/java/com/app/flight/FlightServiceApplication.java
@@ -2,8 +2,10 @@ package com.app.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.app.project.entity"})
 public class FlightServiceApplication {
 
 	public static void main(String[] args) {
